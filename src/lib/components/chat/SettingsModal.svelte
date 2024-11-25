6cab7db14ae01be05100a54b2d3e209bf10e6dd4
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-11-25 20:22:37 +0400

    Update SettingsModal.svelte

--- src/lib/components/chat/SettingsModal.svelte
+++ src/lib/components/chat/SettingsModal.svelte
@@ -13,291 +13,6 @@
 
 	export let show = false;
 
-	interface SettingsTab {
-		id: string;
-		title: string;
-		keywords: string[];
-	}
-
-	const searchData: SettingsTab[] = [
-		{
-			id: 'general',
-			title: 'General',
-			keywords: [
-				'general',
-				'theme',
-				'language',
-				'notifications',
-				'system',
-				'systemprompt',
-				'prompt',
-				'advanced',
-				'settings',
-				'defaultsettings',
-				'configuration',
-				'systemsettings',
-				'notificationsettings',
-				'systempromptconfig',
-				'languageoptions',
-				'defaultparameters',
-				'systemparameters'
-			]
-		},
-		{
-			id: 'interface',
-			title: 'Interface',
-			keywords: [
-				'defaultmodel',
-				'selectmodel',
-				'ui',
-				'userinterface',
-				'display',
-				'layout',
-				'design',
-				'landingpage',
-				'landingpagemode',
-				'default',
-				'chat',
-				'chatbubble',
-				'chatui',
-				'username',
-				'showusername',
-				'displayusername',
-				'widescreen',
-				'widescreenmode',
-				'fullscreen',
-				'expandmode',
-				'chatdirection',
-				'lefttoright',
-				'ltr',
-				'righttoleft',
-				'rtl',
-				'notifications',
-				'toast',
-				'toastnotifications',
-				'largechunks',
-				'streamlargechunks',
-				'scroll',
-				'scrollonbranchchange',
-				'scrollbehavior',
-				'richtext',
-				'richtextinput',
-				'background',
-				'chatbackground',
-				'chatbackgroundimage',
-				'backgroundimage',
-				'uploadbackground',
-				'resetbackground',
-				'titleautogen',
-				'titleautogeneration',
-				'autotitle',
-				'chattags',
-				'autochattags',
-				'responseautocopy',
-				'clipboard',
-				'location',
-				'userlocation',
-				'userlocationaccess',
-				'haptic',
-				'hapticfeedback',
-				'vibration',
-				'voice',
-				'voicecontrol',
-				'voiceinterruption',
-				'call',
-				'emojis',
-				'displayemoji',
-				'save',
-				'interfaceoptions',
-				'interfacecustomization'
-			]
-		},
-		{
-			id: 'personalization',
-			title: 'Personalization',
-			keywords: [
-				'personalization',
-				'memory',
-				'personalize',
-				'preferences',
-				'profile',
-				'personalsettings',
-				'customsettings',
-				'userpreferences',
-				'accountpreferences'
-			]
-		},
-		{
-			id: 'audio',
-			title: 'Audio',
-			keywords: [
-				'audio',
-				'sound',
-				'soundsettings',
-				'audiocontrol',
-				'volume',
-				'speech',
-				'speechrecognition',
-				'stt',
-				'speechtotext',
-				'tts',
-				'texttospeech',
-				'playback',
-				'playbackspeed',
-				'voiceplayback',
-				'speechplayback',
-				'audiooutput',
-				'speechengine',
-				'voicecontrol',
-				'audioplayback',
-				'transcription',
-				'autotranscribe',
-				'autosend',
-				'speechsettings',
-				'audiovoice',
-				'voiceoptions',
-				'setvoice',
-				'nonlocalvoices',
-				'savesettings',
-				'audioconfig',
-				'speechconfig',
-				'voicerecognition',
-				'speechsynthesis',
-				'speechmode',
-				'voicespeed',
-				'speechrate',
-				'speechspeed',
-				'audioinput',
-				'audiofeatures',
-				'voicemodes'
-			]
-		},
-		{
-			id: 'chats',
-			title: 'Chats',
-			keywords: [
-				'chat',
-				'messages',
-				'conversations',
-				'chatsettings',
-				'history',
-				'chathistory',
-				'messagehistory',
-				'messagearchive',
-				'convo',
-				'chats',
-				'conversationhistory',
-				'exportmessages',
-				'chatactivity'
-			]
-		},
-		{
-			id: 'account',
-			title: 'Account',
-			keywords: [
-				'account',
-				'profile',
-				'security',
-				'privacy',
-				'settings',
-				'login',
-				'useraccount',
-				'userdata',
-				'api',
-				'apikey',
-				'userprofile',
-				'profiledetails',
-				'accountsettings',
-				'accountpreferences',
-				'securitysettings',
-				'privacysettings'
-			]
-		},
-		{
-			id: 'admin',
-			title: 'Admin',
-			keywords: [
-				'admin',
-				'administrator',
-				'adminsettings',
-				'adminpanel',
-				'systemadmin',
-				'administratoraccess',
-				'systemcontrol',
-				'manage',
-				'management',
-				'admincontrols',
-				'adminfeatures',
-				'usercontrol',
-				'arenamodel',
-				'evaluations',
-				'websearch',
-				'database',
-				'pipelines',
-				'images',
-				'audio',
-				'documents',
-				'rag',
-				'models',
-				'ollama',
-				'openai',
-				'users'
-			]
-		},
-		{
-			id: 'about',
-			title: 'About',
-			keywords: [
-				'about',
-				'info',
-				'information',
-				'version',
-				'documentation',
-				'help',
-				'support',
-				'details',
-				'aboutus',
-				'softwareinfo',
-				'timothyjaeryangbaek',
-				'openwebui',
-				'release',
-				'updates',
-				'updateinfo',
-				'versioninfo',
-				'aboutapp',
-				'terms',
-				'termsandconditions',
-				'contact',
-				'aboutpage'
-			]
-		}
-	];
-
-	let search = '';
-	let visibleTabs = searchData.map((tab) => tab.id);
-	let searchDebounceTimeout;
-
-	const searchSettings = (query: string): string[] => {
-		const lowerCaseQuery = query.toLowerCase().trim();
-		return searchData
-			.filter(
-				(tab) =>
-					tab.title.toLowerCase().includes(lowerCaseQuery) ||
-					tab.keywords.some((keyword) => keyword.includes(lowerCaseQuery))
-			)
-			.map((tab) => tab.id);
-	};
-
-	const searchDebounceHandler = () => {
-		clearTimeout(searchDebounceTimeout);
-		searchDebounceTimeout = setTimeout(() => {
-			visibleTabs = searchSettings(search);
-			if (visibleTabs.length > 0 && !visibleTabs.includes(selectedTab)) {
-				selectedTab = visibleTabs[0];
-			}
-		}, 100);
-	};
-
 	const saveSettings = async (updated) => {
 		console.log(updated);
 		await settings.set({ ...$settings, ...updated });
@@ -343,7 +58,7 @@
 	}
 </script>
 
-<Modal size="xl" bind:show>
+<Modal bind:show>
 	<div class="text-gray-700 dark:text-gray-100">
 		<div class=" flex justify-between dark:text-gray-300 px-5 pt-4 pb-1">
 			<div class=" text-lg font-medium self-center">{$i18n.t('Settings')}</div>
@@ -366,14 +81,33 @@
 			</button>
 		</div>
 
-		<div class="flex flex-col md:flex-row w-full px-4 pt-1 pb-4 md:space-x-4">
+		<div class="flex flex-col md:flex-row w-full px-4 pt-2 pb-4 md:space-x-4">
 			<div
 				id="settings-tabs-container"
-				class="tabs flex flex-row overflow-x-auto gap-2.5 md:gap-1 md:flex-col flex-1 md:flex-none md:w-40 dark:text-gray-200 text-sm font-medium text-left mb-1 md:mb-0 -translate-y-1"
+				class="tabs flex flex-row overflow-x-auto space-x-1 md:space-x-0 md:space-y-1 md:flex-col flex-1 md:flex-none md:w-40 dark:text-gray-200 text-xs text-left mb-3 md:mb-0"
 			>
-				<div class="hidden md:flex w-full rounded-xl -mb-1 px-0.5 gap-2" id="settings-search">
-					<div class="self-center rounded-l-xl bg-transparent">
-						<Search className="size-3.5" />
+				<button
+					class="px-2.5 py-2 min-w-fit rounded-lg flex-1 md:flex-none flex text-left transition {selectedTab ===
+					'general'
+						? 'bg-gray-100 dark:bg-gray-800'
+						: ' hover:bg-gray-100 dark:hover:bg-gray-850'}"
+					on:click={() => {
+						selectedTab = 'general';
+					}}
+				>
+					<div class=" self-center mr-2">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="w-4 h-4"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M8.34 1.804A1 1 0 019.32 1h1.36a1 1 0 01.98.804l.295 1.473c.497.144.971.342 1.416.587l1.25-.834a1 1 0 011.262.125l.962.962a1 1 0 01.125 1.262l-.834 1.25c.245.445.443.919.587 1.416l1.473.294a1 1 0 01.804.98v1.361a1 1 0 01-.804.98l-1.473.295a6.95 6.95 0 01-.587 1.416l.834 1.25a1 1 0 01-.125 1.262l-.962.962a1 1 0 01-1.262.125l-1.25-.834a6.953 6.953 0 01-1.416.587l-.294 1.473a1 1 0 01-.98.804H9.32a1 1 0 01-.98-.804l-.295-1.473a6.957 6.957 0 01-1.416-.587l-1.25.834a1 1 0 01-1.262-.125l-.962-.962a1 1 0 01-.125-1.262l.834-1.25a6.957 6.957 0 01-.587-1.416l-1.473-.294A1 1 0 011 10.68V9.32a1 1 0 01.804-.98l1.473-.295c.144-.497.342-.971.587-1.416l-.834-1.25a1 1 0 01.125-1.262l.962-.962A1 1 0 015.38 3.03l1.25.834a6.957 6.957 0 011.416-.587l.294-1.473zM13 10a3 3 0 11-6 0 3 3 0 016 0z"
+								clip-rule="evenodd"
+							/>
+						</svg>
 					</div>
 					<div class=" self-center">{$i18n.t('General')}</div>
 				</button>
